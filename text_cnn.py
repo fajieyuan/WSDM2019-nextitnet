7e106e0fe8e1bbc3abffc7c0806c30c7e08d1050
fage <f.yuan.1@research.gla.ac.uk>
2018-12-06 15:10:32 +0800

    Update text_cnn.py

--- text_cnn.py
+++ text_cnn.py
@@ -6,6 +6,7 @@ class TextCNN(object):
     """
     A CNN for text classification.
     Uses an embedding layer, followed by a convolutional, max-pooling and softmax layer.
+    "https://github.com/dennybritz/cnn-text-classification-tf"
     """
     def __init__(
       self, sequence_length, num_classes, vocab_size,
